97c8024a329b1c3f610624dd20b4435e63387c5b
Maciej Białczak <m.bialczak@protonmail.com>
2022-05-14 21:44:27 +0200

    Enhanded StudentRecord constructor to take 2 args
    StudentRecord constructor now takes first and last name

--- src/StudentRecord/StudentRecord.cpp
+++ src/StudentRecord/StudentRecord.cpp
@@ -1,1 +1,7 @@
 #include "StudentRecord.hpp"
+
+StudentRecord::StudentRecord(std::string firstName, std::string lastName)
+    : first_name_(firstName)
+    , last_name_(lastName)
+{
+}
--- src/StudentRecord/StudentRecord.hpp
+++ src/StudentRecord/StudentRecord.hpp
@@ -1,4 +1,11 @@
 #pragma once
-
+#include <string>
 class StudentRecord
-{};
+{
+  public:
+    StudentRecord(std::string firstName, std::string lastName);
+
+  private:
+    std::string first_name_;
+    std::string last_name_;
+};
