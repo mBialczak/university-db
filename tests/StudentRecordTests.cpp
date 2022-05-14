0e3fa487aa1e36b343dfb57452765f5a0a74757d
Maciej Białczak <m.bialczak@protonmail.com>
2022-05-14 22:19:51 +0200

    Added test for StudentRecord::LastName()
    renamed one fixtures member

--- tests/StudentRecordTests.cpp
+++ tests/StudentRecordTests.cpp
@@ -6,12 +6,17 @@ namespace university::student_record::ut {
 class StudentRecordTests : public ::testing::Test
 {
   protected:
-    StudentRecord record_1_ { "John", "Dickens" };
+    StudentRecord rec1_ { "John", "Dickens" };
 };
 
 TEST_F(StudentRecordTests, FirstNameShouldReturnStudentsFirstName)
 {
-    EXPECT_EQ(record_1_.firstName(), "John");
+    EXPECT_EQ(rec1_.firstName(), "John");
+}
+
+TEST_F(StudentRecordTests, LastNameShouldReturnSutdentsLastName)
+{
+    EXPECT_EQ(rec1_.lastName(), "Dickens");
 }
 
 }   // namespace university::student_record::ut
