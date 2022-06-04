93fd6bcd3bb63fdfa7908867ce274cb199d05e42
Maciej Białczak <m.bialczak@protonmail.com>
2022-06-04 22:18:35 +0200

    WIP

--- src/PeselValidator/PeselValidator.cpp
+++ src/PeselValidator/PeselValidator.cpp
@@ -1,9 +1,41 @@
 #include "PeselValidator.hpp"
 
+#include <algorithm>
+#include <cctype>
+
 namespace pesel_validator {
 
 bool PeselValidator::validatePesel(const std::string& pesel)
 {
-    return false;
+    if (!isValidString(pesel)) {
+        return false;
+    }
+    reset(pesel);
 }
+
+bool PeselValidator::isValidString(const std::string& pesel) const
+{
+    return pesel.length() == pesel_length_constant && hasOnlyDigits(pesel);
+}
+
+bool PeselValidator::hasOnlyDigits(const std::string& pesel) const
+{
+    return std::all_of(pesel.begin(), pesel.end(), isdigit);
+}
+
+void PeselValidator::reset(const std::string& pesel)
+{
+    std::transform(
+        pesel.begin(),
+        pesel.end() - 2,   // we don't want last(control) digit here
+        digits_.begin(),
+        [](const auto ch) {
+            return atoi(&ch);
+        });
+}
+
+int PeselValidator::calcDesiredControlDigit() const
+{
+}
+
 }   // end of namespace pesel_validator
--- src/PeselValidator/PeselValidator.hpp
+++ src/PeselValidator/PeselValidator.hpp
@@ -2,6 +2,7 @@
 
 #include "I_PeselValidator.hpp"
 
+#include <array>
 namespace pesel_validator {
 
 class PeselValidator : public I_PeselValidator
@@ -10,6 +11,21 @@ class PeselValidator : public I_PeselValidator
     virtual ~PeselValidator() override = default;
 
     bool validatePesel(const std::string& pesel) override;
+
+  private:
+    bool isValidString(const std::string& pesel) const;
+    bool hasOnlyDigits(const std::string& pesel) const;
+    void reset(const std::string& pesel);
+    int calcDesiredControlDigit() const;
+
+    // pesel length is constant
+    static constexpr size_t pesel_length_constant = { 11 };
+    // control digits weights
+    static constexpr std::array<int, 10> weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+    // variable first ten digits of PESEL number
+    std::array<int, 10> digits_ = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    // control digit
+    int control_dig_ = 0;
 };
 
 }   // end of namespace pesel_validator
