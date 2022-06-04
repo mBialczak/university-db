62cbea10382f3f4f1e58f149b3205854d5a980a5
Maciej Białczak <m.bialczak@protonmail.com>
2022-06-04 15:54:16 +0200

    Added second record for testing purposes

--- tests/StudentRecordTests.cpp
+++ tests/StudentRecordTests.cpp
@@ -9,40 +9,51 @@ class StudentRecordTests : public ::testing::Test
     StudentRecordTests();
 
   protected:
-    StudentRecord rec1_;
+    StudentRecord john;
+    StudentRecord sally;
 };
 
 StudentRecordTests::StudentRecordTests()
-    : rec1_(1ul,
-            "John",
-            "Dickens",
-            "England, London, Puddle of Mudd st. 37",
-            Gender::male)
+    : john(1ul,
+           "John",
+           "Dickens",
+           "England, London, Puddle of Mudd st. 37",
+           Gender::male)
+    , sally(5ul,
+            "Sally",
+            "Smith",
+            "Australia, Sydney, Long Shore st. 22",
+            Gender::female)
 {
 }
 
 TEST_F(StudentRecordTests, FirstNameShouldReturnStudentsFirstName)
 {
-    EXPECT_EQ(rec1_.firstName(), "John");
+    EXPECT_EQ(john.firstName(), "John");
+    EXPECT_EQ(sally.firstName(), "Sally");
 }
 
 TEST_F(StudentRecordTests, LastNameShouldReturnSutdentsLastName)
 {
-    EXPECT_EQ(rec1_.lastName(), "Dickens");
+    EXPECT_EQ(john.lastName(), "Dickens");
+    EXPECT_EQ(sally.lastName(), "Smith");
 }
 
 TEST_F(StudentRecordTests, AddressShouldReturnStudentsAddres)
 {
-    EXPECT_EQ(rec1_.address(), "England, London, Puddle of Mudd st. 37");
+    EXPECT_EQ(john.address(), "England, London, Puddle of Mudd st. 37");
+    EXPECT_EQ(sally.address(), "Australia, Sydney, Long Shore st. 22");
 }
 
 TEST_F(StudentRecordTests, IndexShouldReturnIndexNumber)
 {
-    EXPECT_EQ(rec1_.index(), 1u);
+    EXPECT_EQ(john.index(), 1ul);
+    EXPECT_EQ(sally.index(), 5ul);
 }
 
 TEST_F(StudentRecordTests, GenderShouldReturnStudentsGender)
 {
-    EXPECT_EQ(rec1_.gender(), Gender::male);
+    EXPECT_EQ(john.gender(), Gender::male);
+    EXPECT_EQ(sally.gender(), Gender::female);
 }
 }   // namespace university::student_record::ut
