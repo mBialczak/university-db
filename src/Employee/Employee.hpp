4398d1b7124cfdc933b3339d7fd2723b00f7b823
Maciej Białczak <m.bialczak@protonmail.com>
2022-07-30 15:58:13 +0200

    added Employee class and basic tests

---
+++ src/Employee/Employee.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Person/Person.hpp"
+
+namespace university::employee {
+
+class Employee : public person::Person
+{
+  public:
+    Employee(const std::string& id,
+             const std::string& firstName,
+             const std::string& lastName,
+             const std::string& pesel,
+             const std::string& address,
+             person::Gender gender,
+             double salary);
+    // getters
+    std::string id() const;
+    double salary() const;
+    // setters
+    void setSalary(double newSalary);
+
+    bool operator==(const Employee&) const = default;
+
+  private:
+    std::string id_;
+    double salary_;
+};
+
+std::ostream& operator<<(std::ostream& os, const Employee& student);
+}   // namespace university::employee
