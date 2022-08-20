a4c12f6e5303c5767b5d006176d3087b0da0ad45
Maciej Białczak <m.bialczak@protonmail.com>
2022-08-20 21:08:42 +0200

    added/updated CMakeLists and added starter files

---
+++ tests/DBdataGeneratorTests/DBdataGenearotor-UT.cpp
@@ -0,0 +1,17 @@
+// TODO: VERIFY all includes
+#include "Employee/Employee.hpp"
+#include "Student/Student.hpp"
+#include "UniversityDB/UniversityDB.hpp"
+// TODO: REMOVE
+// #include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include <filesystem>
+
+namespace data_generator::ut {
+// TODO: VERIFY
+//  using employee::Employee;
+//  using person::Gender;
+//  using student::IndexNo;
+//  using student::Student;
+//  using namespace testing;
+}   // namespace data_generator::ut
