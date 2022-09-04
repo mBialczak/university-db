78b77a3a1222bc02ee932a71d2eae03eec9a6c33
Maciej Białczak <m.bialczak@protonmail.com>
2022-09-04 12:28:50 +0200

    Simple main app implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
-int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[])
+// TODO: VERIFY
+#include "DBdataGenerator/DBdataGenerator.hpp"
+#include "UniversityDB/UniversityDB.hpp"
+
+#include <iostream>
+
+int main()
 {
+    using university::UniversityDB;
+    using university::data_generator::DBdataGenerator;
+
+    UniversityDB data_base;
+    DBdataGenerator generator(data_base);
+
+    generator.addStudents(200);
+    generator.addEmployees(25);
+
+    data_base.sortByLastName();
+    data_base.display();
+
+    std::cout << "###################\n"
+              << "Final data base size: " << data_base.size() << std::endl;
+
     return 0;
 }
